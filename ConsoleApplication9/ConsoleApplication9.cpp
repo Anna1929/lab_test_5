7c3ea15a46a4997b174c677acfbb339dedf60ace
Anna1929 <annakoteika18072005@gmail.com>
2022-11-30 13:19:13 +0200

    Init application

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,128 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <fstream>
+using namespace std;
+
+void checkValidInput() 
+{
+	if (cin.fail())
+	{
+		throw "Помилка введення";
+	}
+}
+
+void checkValidParams(int n, double h, double a, double b)
+{
+	if (n < 3 || h <= 0 || a > b)
+	{
+		throw "Невірні дані";
+	}
+}
+
+double calculate(double x, int n) 
+{
+	double y = 0;
+
+	if (x < 0)
+	{
+		for (int i = 1; i <= n - 1; i++)
+		{
+			double sum = 0;
+			for (double j = 1; j <= n; j++)
+			{
+				sum += j / (j * j + j);
+			}
+			y += sum;
+		}
+	}
+	else if (x >= 0)
+	{
+		double sum = 0;
+		for (int i = 1; i <= n - 1; i++)
+		{
+			sum += i;
+		}
+		y = x - sum;
+	}
+	return y;
+}
+
+void output(double x, int n, double a, double b, double h) 
+{
+	for (x = a; x <= b; x += h)
+	{
+		cout << "\nx= " << x << "\t" << "y = " << calculate(x, n);
+	}
+}
+
+void file_is_open(double a, double b, double h, double x, int n) //відкриття файлу для запису даних
+{
+	ofstream fout;
+	fout.open("text.txt");
+	if (!fout.is_open()) //перевірка на відкриття файлу
+	{
+		throw "Файл не відкрився!";
+	}
+	else
+	{ 
+		fout << "a= " << a << "\t" << "b= " << b << endl; //запис даних та результатів
+		fout << "h= " << h << endl;
+		fout << "n= " << n << endl;
+		while (x <= b)
+		{
+			fout << "x=" << x << "\ty=" << calculate(x, n) << endl;
+			x += h;
+		}
+		cout << "Результати занесені до файлу" << endl;
+	}
+}
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	double a, b, h, x;
+	int n;
+	try
+	{
+		cout << "Ввести a, b: "; 
+		cin >> a >> b;
+		checkValidInput();
+
+		cout << "Ввести h: "; 
+		cin >> h;
+		checkValidInput();
+
+		cout << "Ввести n: "; 
+		cin >> n;
+		checkValidInput();
+
+		checkValidParams(n, h, a, b);
+		x = a;
+		calculate(x, n);
+		output(x, n, a, b, h);
+
+		char text;
+		cout << "\nЗаписати результати в файл?(y/n)" << endl;
+		cin >> text;
+
+		if (text == 'y')
+		{
+			file_is_open(a, b, h, x, n);
+		}
+		else
+		{
+			cout << "Результати не занесені до файлу" << endl;
+		}
+	}
+	catch (const char* ex) 
+	{
+		cout << ex << endl;
+		return -1;
+	}
+	catch (...)
+	{
+		cout << "Невідома помилка" << endl;
+		return -2;
+	}
+
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,63 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "D:\Загрузки\Visual Studio\Project\ConsoleApplication9\ConsoleApplication9\ConsoleApplication9.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(calculate_Test1) //Unit тести для функції calculate
+	{
+	public:
+
+		TEST_METHOD(calculate_get1and5_minus9returned) //перевірка при x=1, n=5 з помилкою
+		{
+			double x = 1;
+			int n = 5;
+			double expected = 34; //невірне значення
+			double actual = calculate1(x, n);
+			Assert::AreEqual(expected, actual); 
+		}
+		TEST_METHOD(calculate_get2and3_returned2) //перевірка при x=2, n=3
+		{
+			double x = 2;
+			int n = 3;
+			double expected = -1;
+			double actual = calculate1(x, n);
+			Assert::AreEqual(expected, actual);
+		}
+	};
+	TEST_CLASS(checkValidParams_Test2)//Unit тести для функції checkValidParams
+	{
+	public:
+
+		TEST_METHOD(checkValidParams_get5and1and2and6_exceptionNotThrown)//перевірка з помилкою 
+		{
+			int n = 5;
+			double h = -1, a = 7, b = 6; //помилкові данні вірні
+			try
+			{
+				checkValidParams(n, h, a, b);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+		TEST_METHOD(checkValidParams_get5and0and2and6_exceptionNotThrown)//перевірка даних (h=0)
+		{
+			int n = 5;
+			double h = 0, a = 2, b = 6;
+			try
+			{
+				checkValidParams(n, h, a, b);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	};
+}
